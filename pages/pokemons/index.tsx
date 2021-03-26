303c7340f165ed9dd70bc8169e4ff9e34dfc006a
Neznayka <bugayow.andrei2014@gmail.com>
2021-03-26 18:06:53 +0200

    ''

--- pages/pokemons/index.tsx
+++ pages/pokemons/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { MainLayout } from '@md-shared/layouts/main';
 import PokemonsContainer from '@md-star-wars/pokemons';
+import { GetServerSideProps } from 'next';
+import { createAPI } from '@md-shared/services/api';
+import { initializeStore } from '../../lib/redux/initStore';
+import axios from 'axios';
+import { setGetPokemonsAction } from '../../store/modules/pokemons';
 
 const PokemonsPage = () => {
   return (
@@ -8,6 +13,20 @@ const PokemonsPage = () => {
       <PokemonsContainer />
     </MainLayout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const api = createAPI();
+  const reduxStore = initializeStore();
+
+  const { dispatch } = reduxStore;
+  try {
+    const data = await axios.get('https://pokeapi.co/api/v2/pokemon');
+    dispatch(setGetPokemonsAction(data.data.results));
+  } catch (err) {
+    console.log(err);
+  }
+  return { props: { initialReduxState: JSON.parse(JSON.stringify(reduxStore.getState()))}};
 };
 
 export default PokemonsPage;
